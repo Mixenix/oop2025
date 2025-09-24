1d07d4b9e06d4a49b3f8452e4768334b0ca42742
safin.ai <safin.ai@dozen.mephi.ru>
2025-09-24 19:06:59 +0300

    поправил согласно указаниям

--- 1/syll.cpp
+++ 1/syll.cpp
@@ -27,47 +27,23 @@ const wchar_t* syllabify(const wchar_t* word) {
     if (word == nullptr) {
         throw invalid_argument("Input word is NULL");
     }
-    wchar_t rus_chars[] = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    wchar_t vowels[] = L"аеёиоуыэюяАЕЁИОУЫЭЮЯ";
-    const wchar_t* word_ptr = word;
-
-
-    while (*word_ptr != '\0') {
-        wchar_t* res = wcschr(rus_chars, *word_ptr);
-        if (res == NULL) {
+    wstring wrd = word;
+    wstring rus_chars = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    for (wchar_t c : wrd) {
+        if (rus_chars.find(c) == wstring::npos) {
             throw invalid_argument("Found english characters or numbers in the word!");
         }
-        word_ptr++;
     }
 
-    if (word[0] == L'\0') {
-        wchar_t* result = new wchar_t[1];
-        result[0] = L'\0';
-        return result;
-    }
+    if (wrd.empty()) return L"";
+    wstring result;
+    wstring vowels = L"аеёиоуыэюяАЕЁИОУЫЭЮЯ";
 
-    size_t result_length = 0;
-    for (int i = 0; word[i] != L'\0'; i++) {
-        result_length++;
-        if (wcschr(vowels, word[i]) != NULL && word[i + 1] != L'\0') {
-            result_length += 1;
+    for (int i = 0; i < wrd.length(); i++) {
+        result += wrd[i];
+        if (vowels.find(word[i]) != wstring::npos && i < wrd.length() - 1) {
+            result += L"-";
         }
     }
-
-    wchar_t* result = new wchar_t[result_length + 1];
-    int result_index = 0;
-
-    for (int i = 0; word[i] != L'\0'; i++) {
-        result[result_index++] = word[i];
-
-        if (wcschr(vowels, word[i]) != NULL && word[i + 1] != L'\0') {
-            result[result_index++] = L'-';
-        }
-    }
-
-    result[result_index] = L'\0';
-    const wchar_t* res = (const wchar_t*)result;
-    // delete[] result;
-    return res;
-    
+    return result.c_str();
 }
